426d9e73c3c10cddc21e5604bd9f5291790bb099
Mark Hardy <mark@ascentapps.com>
2019-05-10 14:07:17 -0700

    another Like fix

--- vanilla_service.js
+++ vanilla_service.js
@@ -135,7 +135,7 @@ app.get('/search', function (req, res) {
 	const search = req.query.search;
 	var json = {};
 
-	var sql_query = "SELECT * FROM item_template i WHERE i.name = '" + search + "' ORDER BY i.Quality DESC, i.name ASC";
+	var sql_query = "SELECT * FROM item_template i WHERE i.name LIKE '" + search + "' ORDER BY i.Quality DESC, i.name ASC";
 
 	var result = connection.query(sql_query, function(err, result, fields) {
 		if (err) throw err;
@@ -173,7 +173,7 @@ app.get('/item', function (req, res) {
 	const item_id = req.query.item_id;
 	var json = {};
 
-	var sql_query = "SELECT * FROM item_template i WHERE i.entry LIKE '" + item_id + "'";
+	var sql_query = "SELECT * FROM item_template i WHERE i.entry = '" + item_id + "'";
 
 	var result = connection.query(sql_query, function(err, result, fields) {
 		if (err) throw err;
